171332952e48f6d7ecc9b5bb25c8dd6cdd7e6bce
Archer31-code <penequitotrishiamae630@gmail.com>
2026-01-09 13:27:32 +0800

    changed CorsConfig to allow api requests

--- src/main/java/com/resumegenerator/output/config/CorsConfig.java
+++ src/main/java/com/resumegenerator/output/config/CorsConfig.java
@@ -4,19 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    @Configuration
-    public class CorsConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/**")
-                    .allowedOrigins(
-                            "http://localhost:8080",
-                            "http://localhost:3306",
-                            "http://localhost:3000",
-                            "https://resume-generator-next-app.vercel.app/"
-                    )
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-        }
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")                  // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
+}
